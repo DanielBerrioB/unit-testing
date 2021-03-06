c6c8dc510c0a6ae063117e69166f2c3747322434
Daniel Berrío Barrera <danielberriobarrera@Daniels-MacBook-Pro.local>
2021-03-06 12:44:20 -0500

    Feat(Fix): Fix tests config

--- src/app/image-details/image-details.component.spec.ts
+++ src/app/image-details/image-details.component.spec.ts
@@ -1,19 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ImageDetailComponent } from './image-details.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ImageService } from '../image.service';
 
 describe('ImageDetailsComponent', () => {
   let component: ImageDetailComponent;
   let fixture: ComponentFixture<ImageDetailComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ImageDetailComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
+      declarations: [ ImageDetailComponent],
+      providers: [ ImageService ],
+    });
     fixture = TestBed.createComponent(ImageDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,19 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GalleryComponent } from './image-gallery.component';
+import { ImageService } from '../image.service';
+import { FilterimagesPipe } from '../filterimages.pipe';
 
 describe('ImageGalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [GalleryComponent, FilterimagesPipe],
+      providers: [ImageService, FilterimagesPipe],
+    });
     fixture = TestBed.createComponent(GalleryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +19,12 @@ describe('ImageGalleryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Debe crear las 5 imagenes y mostrarlas en pantalla', () => {
+    const fixture = TestBed.createComponent(GalleryComponent);
+    const image1 = 'perro1.jpg';
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
   });
 });
