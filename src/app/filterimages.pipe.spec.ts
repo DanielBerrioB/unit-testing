2543b946d9a12de6e6e0f7f8f1c2c27f03dd0349
Daniel Berrío Barrera <danielberriobarrera@Daniels-MacBook-Pro.local>
2021-03-13 12:32:04 -0500

    Feat(Test): Add test for pipe and for image gallery

--- src/app/filterimages.pipe.spec.ts
+++ src/app/filterimages.pipe.spec.ts
@@ -1,8 +1,26 @@
 import { FilterimagesPipe } from './filterimages.pipe';
+import { ImageService } from './image.service';
 
 describe('FilterimagesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterimagesPipe();
+  let pipe: FilterimagesPipe;
+  let service: ImageService;
+  const items = [];
+
+
+  beforeAll(() => {
+    pipe = new FilterimagesPipe();
+    service = new ImageService();
+    service.getImages().forEach(element => {
+      items.push(element);
+    });
+  });
+
+  it('Debe crear la instancia del pipe', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('Debe filtrar por la etiqueta all y retornar todos los elementos', () => {
+    const filteredResult = pipe.transform(items, 'all');
+    expect(filteredResult.length).toBe(5);
   });
 });
